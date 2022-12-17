fb5851e25912d3586b468d975e0dcf0844a834ec
Nataliia-Kalinina <nnatta.od@gmail.com>
2022-12-17 16:26:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* System.out.println("Hello world!");
+        /*
+                ! не
+                & и
+                | или       */
+
+     /*   int k = 0;
+        while (k<5)
+        {
+            System.out.println(k);
+            k++;
+        }
+     */
+      /*  int k = 0;
+        while (k<17)
+        {
+            System.out.println("я люблю выходные");
+            k++;
+        } */
+
+        /* Scanner console = new Scanner(System.in);
+
+        String s = console.nextLine();
+
+        System.out.println("My string " +s );*/
+
+
+       /* while (true)
+        {
+            String s_new = console.nextLine();
+            if (s_new.equals("exit"))
+            {
+                break;
+            }
+        }
+        */
+
+       /* int i  = 17;
+        do {
+            System.out.println("привет");
+            i--;
+        } while (i>0);
+
+        System.out.println("________");
+
+        int l  = 5;
+        while (l>0)
+        {
+            System.out.println(l);
+            l--;
+        }*/
+
+      /*  int result = 0;
+        for (int i = 0; i<=50; i++) {
+
+            if ((i % 5) == 0) {
+                System.out.println(i);
+                continue;
+            }
+
+            result = result + i;
+        } */
+
+        Scanner console = new Scanner(System.in);
+        String s = console.nextLine();
+        int s_1 = Integer.parseInt(s);
+
+        System.out.println(s+1);
+        System.out.println(s_1+1);
+
+
+
+
+    }
+}
